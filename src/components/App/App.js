31796801b15a78ebb5ae42c11470b99e11bf5583
Audrey-Costa <audreycosta@nano.ufrj.br>
2022-07-06 16:18:39 -0300

    Feature login implement

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,14 @@ import '../../css/styles.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Register_Page from '../Register_Page/Register_Page';
+import LoginPage from '../Login/LoginPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register_Page />} />
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/sign-up" element={<Register_Page />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Register_Page/Register_Page.js
+++ src/components/Register_Page/Register_Page.js
@@ -3,13 +3,15 @@ import InputsRegister from './InputsRegister';
 import ButtonOnlyWords from '../../shared/ButtonOnlyWords.js';
 import logo from '../../assets/logo.png';
 import styled from 'styled-components';
+import {Link} from 'react-router-dom'
+
 export default function Register_Page() {
   return (
     <>
       <Main margin_top={'35'}>
         <Logo src={logo} alt="" />
         <InputsRegister />
-        <ButtonOnlyWords>Já possui cadastro? Faça login!</ButtonOnlyWords>
+        <Link to='/'><ButtonOnlyWords>Já possui cadastro? Faça login!</ButtonOnlyWords></Link>
       </Main>
     </>
   );
