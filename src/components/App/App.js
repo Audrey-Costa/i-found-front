645d531ed406904329d74b9a2cd928f44ed4e88c
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-07-10 18:04:52 -0300

    terminada tela de registro de produtos

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,22 +14,28 @@ import LoginPage from '../Login/LoginPage';
 import HomePage from '../Home_Page/HomePage';
 import ProductAddPage from '../Products/Product_Add/ProductAddPage';
 import ProductRegisteredPage from '../Products/Product_Add/ProductRegisteredPage';
+import ProductsAndCategoriesContext from '../../contexts/ProductsAndCategoriesContext';
 
 export default function App() {
+  const [productsAndCategories, setProductsAndCategories] = useState(null);
   const [objLoginResponse, setObjLoginResponse] = useState({});
 
   return (
-    <UserContext.Provider value={{ objLoginResponse, setObjLoginResponse }}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/add-product" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/sign-up" element={<RegisterPage />} />
-          <Route path="/" element={<ProductAddPage />} />
+    <ProductsAndCategoriesContext.Provider
+      value={{ productsAndCategories, setProductsAndCategories }}
+    >
+      <UserContext.Provider value={{ objLoginResponse, setObjLoginResponse }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/add-product" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/sign-up" element={<RegisterPage />} />
+            <Route path="/" element={<ProductAddPage />} />
 
-          <Route path="/sucessProductRegister" element={<ProductRegisteredPage />} />
-        </Routes>
-      </BrowserRouter>
-    </UserContext.Provider>
+            <Route path="/sucessProductRegister" element={<ProductRegisteredPage />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
+    </ProductsAndCategoriesContext.Provider>
   );
 }
